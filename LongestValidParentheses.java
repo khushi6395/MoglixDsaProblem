df3de25d7025dd8685f587382a3ee6f3269040ab
khushi sharma <sharmakhushi15012005@gmail.com>
2026-07-21 13:07:33 +0530

    add java code

---
+++ LongestValidParentheses.java
@@ -0,0 +1,38 @@
+package problem;
+
+import java.util.Stack;
+
+public class LongestValidParentheses {
+
+    public static int longestParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(longestParentheses("(()"));        
+        System.out.println(longestParentheses(")()(()))"));     
+        
+    }
+}
